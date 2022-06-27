bdf38415994b91d2781b811c73748ecdc5f75e50
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-27 00:14:05 -0700

    Update ts-dev-stack

--- test/exports/unpkg.min.test.cjs
+++ test/exports/unpkg.min.test.cjs
@@ -1,27 +1,27 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { assert } = require("chai");
-const { Parser, Part, Response } = require("parser-multipart/dist/umd/parser-multipart.min.js");
-const response = require("../lib/response.cjs");
+const assert = require('assert');
+const { Parser, Part, Response } = require('parser-multipart/dist/umd/parser-multipart.min.js');
+const response = require('../lib/response.cjs');
 
-const json = response("json");
+const json = response('json');
 
-describe("exports parser-multipart/dist/umd/parser-multipart.js", function () {
-  it("MultiData", function () {
-    const parser = new Parser(json.headers["content-type"]);
+describe('exports parser-multipart/dist/umd/parser-multipart.js', function () {
+  it('MultiData', function () {
+    const parser = new Parser(json.headers['content-type']);
     parser.parse(json.body);
     const result = parser.parts.map((part) => part.response.json());
-    assert.deepEqual(result, [{ name: "item1" }, { name: "item2" }]);
+    assert.deepEqual(result, [{ name: 'item1' }, { name: 'item2' }]);
   });
 
-  it("Part", function () {
+  it('Part', function () {
     const part = new Part();
     part.parse(json.parts[0]);
-    assert.deepEqual(part.response.json(), { name: "item1" });
+    assert.deepEqual(part.response.json(), { name: 'item1' });
   });
 
-  it("Response", function () {
-    const response = new Response("application/http");
+  it('Response', function () {
+    const response = new Response('application/http');
     response.parse(json.responses[0]);
-    assert.deepEqual(response.json(), { name: "item1" });
+    assert.deepEqual(response.json(), { name: 'item1' });
   });
 });
