4e43c01549fb24987ccab4e6e57eae00840e5773
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-27 00:14:05 -0700

    Update ts-dev-stack

--- src/Parser.ts
+++ src/Parser.ts
@@ -1,7 +1,7 @@
-import Part from "./Part";
-import parseHeader from "./lib/parseHeader";
-import parseText from "./lib/parseText";
-import type { HeadersObject } from "./index";
+import Part from './Part.js';
+import parseHeader from './lib/parseHeader.js';
+import parseText from './lib/parseText.js';
+import type { HeadersObject } from './index.js';
 
 export enum ParseStatus {
   Parts = 1,
@@ -24,23 +24,23 @@ export default class MultipartParser {
   private boundary: string | null = null;
 
   constructor(headers: Headers | string | HeadersObject) {
-    if (!headers) throw new Error("Headers missing");
+    if (!headers) throw new Error('Headers missing');
 
     let contentType: string;
-    if (typeof headers === "string") contentType = headers;
-    /* c8 ignore start */ else if (headers.get) contentType = (headers as Headers).get("content-type");
-    /* c8 ignore stop */ else contentType = (headers as HeadersObject)["content-type"];
-    if (!contentType) throw Error("content-type header not found");
+    if (typeof headers === 'string') contentType = headers;
+    /* c8 ignore start */ else if (headers.get) contentType = (headers as Headers).get('content-type');
+    /* c8 ignore stop */ else contentType = (headers as HeadersObject)['content-type'];
+    if (!contentType) throw Error('content-type header not found');
 
     const parts = contentType.split(/;/g);
     this.type = parts.shift().trim();
-    if (this.type.indexOf("multipart") !== 0) {
+    if (this.type.indexOf('multipart') !== 0) {
       throw new Error(`Expecting a multipart type. Received: ${contentType}`);
     }
-    for (const part of parts) parseHeader(this.headers, part, "=");
+    parts.forEach(part => parseHeader(this.headers, part, '=')) ;
 
     // boundary
-    if (!this.headers.boundary) throw new Error("Invalid Content Type: no boundary");
+    if (!this.headers.boundary) throw new Error('Invalid Content Type: no boundary');
     this.boundary = `--${this.headers.boundary}`;
     this._parsingState.boundaryEnd = `--${this.headers.boundary}--`;
     this._parsingState.status = ParseStatus.Parts;
@@ -57,7 +57,7 @@ export default class MultipartParser {
   push(line: string): void {
     const part = this.parts.length ? this.parts[this.parts.length - 1] : null;
 
-    if (!this._parsingState) throw new Error("Attempting to parse a completed multipart");
+    if (!this._parsingState) throw new Error('Attempting to parse a completed multipart');
     if (line === null) {
       if (part && !part.done()) part.push(null);
       this._parsingState = null;
