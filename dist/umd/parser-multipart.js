0eefa4f3d155d63d13d77a01c98426192ce9b051
Kevin Malakoff <kmalakoff@gmail.com>
2022-07-20 19:42:27 -0700

    Upgrade modules

--- dist/umd/parser-multipart.js
+++ dist/umd/parser-multipart.js
@@ -19,20 +19,19 @@
    * @param offset The offset for searching
    * @param includeLength Include the length in the return value
    * @returns When includeLength is true, returns a pair of [offset, length] to provide the length of CR (1), LF (1) or CRLF (2)
-   */ function indexOfNewline(string) {
-      var offset = arguments.length > 1 && arguments[1] !== void 0 ? arguments[1] : 0, includeLength = arguments.length > 2 && arguments[2] !== void 0 ? arguments[2] : false;
-      if (offset < 0) throw new Error("Unexpected negative offset");
-      if (offset > string.length) throw new Error("Offset is longer than the string. Offset: ".concat(offset, ". String: ").concat(string.length));
+   */ function indexOfNewline(string, offset = 0, includeLength = false) {
+      if (offset < 0) throw new Error('Unexpected negative offset');
+      if (offset > string.length) throw new Error(`Offset is longer than the string. Offset: ${offset}. String: ${string.length}`);
       while(offset < string.length){
-          var value = string[offset];
-          if (value === "\n") return includeLength ? [
+          const value = string[offset];
+          if (value === '\n') return includeLength ? [
               offset,
               1
           ] : offset;
-          else if (value === "\r") {
+          else if (value === '\r') {
               return includeLength ? [
                   offset,
-                  string[offset + 1] === "\n" ? 2 : 1
+                  string[offset + 1] === '\n' ? 2 : 1
               ] : offset;
           }
           offset++;
