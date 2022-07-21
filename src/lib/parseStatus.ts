046c6f2d21c84a5a3e9b598a1548359f9d4421ab
Kevin Malakoff <kmalakoff@gmail.com>
2022-07-21 13:26:25 -0700

    Tweaks

--- src/lib/parseStatus.ts
+++ src/lib/parseStatus.ts
@@ -15,7 +15,10 @@ export default function parseStatus(result: StatusResult, line: string): boolean
   const match = line.match(statusLine);
   if (!match) return false;
 
-  result.version = { major: parseInt(match[1], 10), minor: parseInt(match[2], 10) };
+  result.version = {
+    major: parseInt(match[1], 10),
+    minor: parseInt(match[2], 10),
+  };
   result.status = parseInt(match[3], 10);
   result.statusText = match[4];
   result.ok = result.statusText === 'OK';
