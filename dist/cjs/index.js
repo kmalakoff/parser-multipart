958fbd0f52b1c58e1c2d9e19731929dffc6220b9
Kevin Malakoff <kmalakoff@gmail.com>
2022-08-15 12:37:31 -0700

    Upgrade modules

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -1,30 +1,36 @@
+// @ts-ignore
 "use strict";
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-Object.defineProperty(exports, "Parser", {
-    enumerable: true,
-    get: function() {
+function _export(target, all) {
+    for(var name in all)Object.defineProperty(target, name, {
+        enumerable: true,
+        get: all[name]
+    });
+}
+_export(exports, {
+    Parser: function() {
         return _parserTs.default;
-    }
-});
-Object.defineProperty(exports, "Part", {
-    enumerable: true,
-    get: function() {
+    },
+    Part: function() {
         return _partTs.default;
-    }
-});
-Object.defineProperty(exports, "Response", {
-    enumerable: true,
-    get: function() {
+    },
+    Response: function() {
         return _responseTs.default;
     }
 });
-var _parserTs = _interopRequireDefault(require("./Parser.js"));
-var _partTs = _interopRequireDefault(require("./Part.js"));
-var _responseTs = _interopRequireDefault(require("./Response.js"));
+var _parserTs = /*#__PURE__*/ _interopRequireDefault(require("./Parser.js"));
+var _partTs = /*#__PURE__*/ _interopRequireDefault(require("./Part.js"));
+var _responseTs = /*#__PURE__*/ _interopRequireDefault(require("./Response.js"));
 function _interopRequireDefault(obj) {
     return obj && obj.__esModule ? obj : {
         default: obj
     };
+}
+
+if ((typeof exports.default === 'function' || (typeof exports.default === 'object' && exports.default !== null)) && typeof exports.default.__esModule === 'undefined') {
+  Object.defineProperty(exports.default, '__esModule', { value: true });
+  for (var key in exports) exports.default[key] = exports[key];
+  module.exports = exports.default;
 }
--- dist/cjs/lib/parseHeader.js
+++ dist/cjs/lib/parseHeader.js
@@ -1,12 +1,24 @@
+// @ts-ignore
 "use strict";
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-module.exports = parseHeader;
+Object.defineProperty(exports, "default", {
+    enumerable: true,
+    get: function() {
+        return parseHeader;
+    }
+});
 function parseHeader(result, line, delimiter) {
     var index = line.indexOf(delimiter);
     if (index === -1) throw new Error("Unexpected header format: ".concat(line));
     var key = line.slice(0, index);
     var value = line.slice(index + 1);
     result[key.trim().toLowerCase()] = value.trim();
+}
+
+if ((typeof exports.default === 'function' || (typeof exports.default === 'object' && exports.default !== null)) && typeof exports.default.__esModule === 'undefined') {
+  Object.defineProperty(exports.default, '__esModule', { value: true });
+  for (var key in exports) exports.default[key] = exports[key];
+  module.exports = exports.default;
 }
