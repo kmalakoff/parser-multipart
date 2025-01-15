3c500a6c36024a61839dc708ae78195558ddb224
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-14 17:55:29 -0800

    Update dev dependencies and configure test runner port

--- wtr.config.mjs
+++ wtr.config.mjs
@@ -2,6 +2,7 @@ import { importMapsPlugin } from '@web/dev-server-import-maps';
 import createConfig from 'tsds-web-test-runner/createConfig.mjs';
 
 export default createConfig({
+  port: 8002,
   plugins: [
     importMapsPlugin({
       inject: {
