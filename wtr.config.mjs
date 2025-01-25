2e7512fa9b049af174228fc6d82ca8efc74b2638
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-24 18:51:02 -0800

    Move types to root and update parser interface

--- wtr.config.mjs
+++ wtr.config.mjs
@@ -2,13 +2,13 @@ import { importMapsPlugin } from '@web/dev-server-import-maps';
 import createConfig from 'tsds-web-test-runner/createConfig.mjs';
 
 export default createConfig({
-  port: 9003,
+  port: 9006,
   plugins: [
     importMapsPlugin({
       inject: {
         importMap: {
           imports: {
-            'pinkie-promise': `https://esm.sh/pinkie-promise@2.0.1`,
+            'pinkie-promise': "https://esm.sh/pinkie-promise@2.0.1"
           },
         },
       },
