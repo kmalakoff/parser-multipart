1e68e3ee484bd7dde8777271ec548c65ea890ef6
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 13:14:37 -0700

    Upgrade to mandatory typescript extensions

--- src/MultipartParser.ts
+++ src/MultipartParser.ts
@@ -1,6 +1,6 @@
-import PartParser from './PartParser';
-import parseHeader from './lib/parseHeader';
-import parseText from './lib/parseText';
+import PartParser from './PartParser.js';
+import parseHeader from './lib/parseHeader.js';
+import parseText from './lib/parseText.js';
 
 export const ParseStatus = {
   Parts: 1,
