b82f087f0e2373e3e9d988e2349db3a2e68372b0
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-19 23:23:39 -0700

    Refactor unit tests and update tsconfig settings

--- test/unit/Response.test.ts
+++ test/unit/Response.test.ts
@@ -2,7 +2,7 @@ import assert from 'assert';
 import newlineIterator from 'newline-iterator';
 // @ts-ignore
 import { Response } from 'parser-multipart';
-import Pinkie from 'pinkie-promise'; // @ts-ignore
+import Pinkie from 'pinkie-promise';
 import response from '../lib/response.ts';
 
 const dataJSON = response([{ name: 'item1' }, { name: 'item2' }]);
@@ -12,16 +12,13 @@ const dataError = response([new Error('failed1'), new Error('failed2')]);
 describe('Response', () => {
   (() => {
     // patch and restore promise
-    const root = typeof window === 'undefined' ? global : window;
-    // @ts-ignore
-    let rootPromise: Promise;
+    if (typeof global === 'undefined') return;
+    const globalPromise = global.Promise;
     before(() => {
-      rootPromise = root.Promise;
-      // @ts-ignore
-      root.Promise = Pinkie;
+      global.Promise = Pinkie;
     });
     after(() => {
-      root.Promise = rootPromise;
+      global.Promise = globalPromise;
     });
   })();
 
